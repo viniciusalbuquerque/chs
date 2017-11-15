1a2809075fbe82bdb7027dd0a99ebb1826284413
Vinicius Albuquerque <vinicius.aalbuquerque@gmail.com>
2017-11-15 10:52:11 -0300

    improv

--- MaxMinSum/mms.cpp
+++ MaxMinSum/mms.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <set>
 #include <iostream>
 #include <algorithm>
 using namespace std;
@@ -9,28 +10,18 @@ int main() {
        cin >> arr[arr_i];
     }
 
-    vector<long int> sums;
+    multiset<long int> sums;
     for(int i = 0; i < arr.size(); i++) {
     	long int sum = 0;
     	for(int j = 0; j < arr.size() - 1; j++) {
     		sum += arr[j];
     	}
-    	sums.push_back(sum);
+    	sums.insert(sum);
     	rotate(arr.begin(), arr.begin()+1, arr.end());
     }
 
-    long int max = 0;
-    long int min = 0;
-    for(int i = 0; i < sums.size(); i++) {
-    	if(i == 0) min = sums[i];
-    	if(sums[i] > max) {
-    		max = sums[i];
-    	}
-    	if(sums[i] < min) {
-    		min = sums[i];
-    	}
-    }
+    
 
-    cout << min << " " << max << endl;
+    cout << *sums.begin() << " " << *sums.rbegin() << endl;
     return 0;
 }
